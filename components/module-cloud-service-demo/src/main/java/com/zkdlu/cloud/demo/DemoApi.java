9c3e7bbfcc9c8d6f6f26a5298539ab32ede9678e
zkdlu <zkdlu159951@gmail.com>
2022-02-27 23:53:19 +0900

    cloud: demo service 추가

--- components/module-cloud-service-demo/src/main/java/com/zkdlu/cloud/demo/DemoApi.java
+++ components/module-cloud-service-demo/src/main/java/com/zkdlu/cloud/demo/DemoApi.java
@@ -1,12 +1,21 @@
 package com.zkdlu.cloud.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class DemoApi {
+    @Lazy
+    private final DiscoveryClient discoveryClient;
+
     @GetMapping("/")
-    public String demo() {
-        return "Hello world";
+    public List<String> demo() {
+        return discoveryClient.getServices();
     }
 }

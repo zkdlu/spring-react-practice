c6fd1b6d18ddacb4ca2025cbd0c1a3e5f040ff98
zkdlu <zkdlu159951@gmail.com>
2022-03-02 23:47:00 +0900

    websocket connected

---
+++ components/module-chatting/src/main/java/com/zkdlu/chat/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.zkdlu.chat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // websocket handshake connection
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // routed by @MessageMapping
+        registry.setApplicationDestinationPrefixes("/pub");
+        // routing to broker for broadcasting
+        registry.enableSimpleBroker("/sub");
+    }
+}

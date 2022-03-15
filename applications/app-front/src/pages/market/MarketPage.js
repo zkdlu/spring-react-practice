75821c6c894c9217a3aa9dda46130415432751c0
zkdlu <zkdlu159951@gmail.com>
2022-03-16 00:08:11 +0900

    front: market api 연동

--- applications/app-front/src/pages/market/MarketPage.js
+++ applications/app-front/src/pages/market/MarketPage.js
@@ -1,18 +1,24 @@
 import { Component } from "react";
+import api from "../../api";
 import MarketTemplate from "./components/MarketTemplate";
 import ProductList from "./components/ProductList";
 
 class MarketPage extends Component {
     state = {
-        products: [
-            {
-                id: 1,
-                name: 'product-name',
-                price: 1000,
-                stock: 10
-            }
-        ]
+        products: []
+    }
 
+    async componentDidMount() {
+        await this.fetchProducts();
+    }
+
+    async fetchProducts() {
+        const fetchedProducts = await api.get('products');
+
+        console.log(fetchedProducts);
+        this.setState({
+            products: [...fetchedProducts]
+        })
     }
 
     render() {
--- components/module-market/src/main/java/com/zkdlu/market/Product.java
+++ components/module-market/src/main/java/com/zkdlu/market/Product.java
@@ -1,12 +1,14 @@
 package com.zkdlu.market;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Product {

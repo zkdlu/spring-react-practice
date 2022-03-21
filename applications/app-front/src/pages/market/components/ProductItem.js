831ccae804a5ae46d763b2c828963cb85cf7ee16
zkdlu <zkdlu159951@gmail.com>
2022-03-21 22:27:00 +0900

    front: 상품 이미지 임시

--- applications/app-front/src/pages/market/components/ProductItem.js
+++ applications/app-front/src/pages/market/components/ProductItem.js
@@ -13,10 +13,10 @@ class ProductItem extends Component {
                         {name}
                     </div>
                     <section>
-                        {price}
+                        <img className="product-image" src="https://cdn.011st.com/11dims/resize/800x800/quality/75/11src/dl/22/8/9/6/3/0/9/fXdKt/4232896309_139161764_05.jpg" />
                     </section>
                     <section>
-                        {stock}
+                        {price} 원
                     </section>
                 </div>
             </Link>

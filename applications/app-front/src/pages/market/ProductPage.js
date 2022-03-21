0b78d2ca412d4eb40310686c5414d22a66669b2c
zkdlu <zkdlu159951@gmail.com>
2022-03-21 23:46:00 +0900

    front: react 이거 용하네

--- applications/app-front/src/pages/market/ProductPage.js
+++ applications/app-front/src/pages/market/ProductPage.js
@@ -1,25 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from '../../api'
+import ProductTemplate from "./components/ProductTemplate";
 
 async function fetchProduct(id) {
     const product = await api.get(`products/${id}`);
-
-    console.log(product);
+    return product;
 }
 
 const ProductPage = () => {
     const { id } = useParams();
-    
+    const [product, setProduct] = useState(0);
+
     useEffect(async () => {
-        await fetchProduct(id);
+        const response = await fetchProduct(id);
+        setProduct(response);
     }, []);
-    
 
     return (
-        <div>
-            product
-        </div>
+        <ProductTemplate product={product}>
+            <img className="product-image" src="https://cdn.011st.com/11dims/resize/640x640/quality/75/11src/dl/22/1/6/2/1/5/2/YvAQE/3736162152_139127350_05.jpg" />
+        </ProductTemplate>
     )
 }
 
